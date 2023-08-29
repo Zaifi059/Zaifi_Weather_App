3362c463a10b43233fe944440c8fbdcbd35cbed8
Choudhary Huzaifa <royalprince5959@gmail.com>
2023-08-29 23:37:07 +0500

    Latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(WeatherApp());
@@ -14,7 +14,6 @@ class WeatherApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Zaifi Weather App',
       theme: ThemeData(
-
         primarySwatch: Colors.indigo,
       ),
       home: WeatherPage(),
@@ -32,29 +31,37 @@ class _WeatherPageState extends State<WeatherPage> {
   String city = 'Vehari';
   String temperature = '';
   String currentWeather = '';
+  String humidity = '';
+  List dailyForecastData = [];
 
   Future<void> _fetchWeatherData() async {
+    final geocodingUrl = Uri.parse(
+        'https://api.openweathermap.org/geo/1.0/direct?q=$city&limit=1&appid=$apiKey');
+    final geocodingResponse = await http.get(geocodingUrl);
+    final geocodingData = json.decode(geocodingResponse.body);
+    final lat = geocodingData[0]['lat'];
+    final lon = geocodingData[0]['lon'];
+
     final url = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric');
+        'https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=minutely,hourly,alerts&appid=$apiKey&units=metric');
 
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
       final weatherData = json.decode(response.body);
-      final mainData = weatherData['main'];
-      final weather = weatherData['weather'][0]['main'];
+      final mainData = weatherData['current'];
+      final weather = weatherData['current']['weather'][0]['main'];
+      dailyForecastData = weatherData['daily'];
 
       setState(() {
         temperature = '${mainData['temp']}°C';
         currentWeather = weather;
+        humidity = '${mainData['humidity']}%';
       });
-    } else
-    {
+    } else {
       // Handle error
     }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -119,24 +126,64 @@ class _WeatherPageState extends State<WeatherPage> {
                   ),
                 ],
               ),
-              SizedBox(height: 16),
-              FloatingActionButton(
-                onPressed: _fetchWeatherData,
-                child: Icon(Icons.auto_fix_normal_sharp),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Temperature:',
-                style: TextStyle(fontSize: 20),
-                selectionColor: Colors.redAccent,
-              ),
-              SizedBox(height: 16),
-              Text(
-                temperature,
-                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16),
-
+              SizedBox(height:
+              16),
+              FloatingActionButton(onPressed:
+              _fetchWeatherData, child:
+              Icon(Icons.auto_fix_normal_sharp)),
+              SizedBox(height:
+              16),
+              Text('Temperature:',
+                  style:
+                  TextStyle(fontSize:
+                  20)),
+              SizedBox(height:
+              16),
+              Text(temperature,
+                  style:
+                  TextStyle(fontSize:
+                  36, fontWeight:
+                  FontWeight.bold)),
+              SizedBox(height:
+              16),
+              Text('Humidity:',
+                  style:
+                  TextStyle(fontSize:
+                  20)),
+              SizedBox(height:
+              16),
+              Text(humidity,
+                  style:
+                  TextStyle(fontSize:
+                  36, fontWeight:
+                  FontWeight.bold)),
+              SizedBox(height:
+              16),
+              Text('Daily Forecast:',
+                  style:
+                  TextStyle(fontSize:
+                  20)),
+              Expanded(child:
+              ListView.builder(itemBuilder:
+                  (context, index) {
+                final time =
+                DateTime.fromMillisecondsSinceEpoch(dailyForecastData[index]['dt'] * 1000);
+                final day =
+                DateFormat('EEEE').format(time);
+                return ListTile(title:
+                Row(mainAxisAlignment:
+                MainAxisAlignment.center, children:
+                [
+                  Text(day,
+                      style:
+                      TextStyle(fontWeight:
+                      FontWeight.bold)),
+                  SizedBox(width:
+                  16),
+                  Text('${dailyForecastData[index]['temp']['day']}°C'),
+                ]));
+              }, itemCount:
+              dailyForecastData.length))
             ],
           ),
         ),
