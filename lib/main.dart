01145657003069758c4baf832d47e556605757c2
Choudhary Huzaifa <royalprince5959@gmail.com>
2023-08-29 21:59:27 +0500

    Apt

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,20 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+
 void main() {
   runApp(WeatherApp());
-
 }
 
 class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Zaifi Weather App',
       theme: ThemeData(
-        backgroundColor: Colors.white12,
-        primarySwatch: Colors.deepPurple,
+
+        primarySwatch: Colors.indigo,
       ),
       home: WeatherPage(),
     );
@@ -28,25 +29,32 @@ class WeatherPage extends StatefulWidget {
 
 class _WeatherPageState extends State<WeatherPage> {
   final apiKey = '158967843a4e4cf212969a5b6480d58c';
-  String city = '';
+  String city = 'Vehari';
   String temperature = '';
+  String currentWeather = '';
 
   Future<void> _fetchWeatherData() async {
-    final url =
-    Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric');
+    final url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric');
 
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
       final weatherData = json.decode(response.body);
       final mainData = weatherData['main'];
+      final weather = weatherData['weather'][0]['main'];
+
       setState(() {
         temperature = '${mainData['temp']}°C';
+        currentWeather = weather;
       });
-    } else {
+    } else
+    {
       // Handle error
     }
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -54,40 +62,83 @@ class _WeatherPageState extends State<WeatherPage> {
       appBar: AppBar(
         title: Text('Zaifi Weather App'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  city = value;
-                });
-              },
-              decoration: InputDecoration(
-                labelText: 'Enter City',
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.lightBlueAccent, Colors.deepPurpleAccent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    city = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Enter City',
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                _fetchWeatherData();
-              },
-              child: Text('Get Weather'),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Temperature:',
-              style: TextStyle(fontSize: 20),
-              selectionColor: Colors.redAccent,
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      setState(() {
+                        city = 'Vehari';
+                      });
+                      _fetchWeatherData();
+                    },
+                    child: Text('V'),
+                  ),
+                  SizedBox(width: 10),
+                  FloatingActionButton(
+                    onPressed: () {
+                      setState(() {
+                        city = 'Multan';
+                      });
+                      _fetchWeatherData();
+                    },
+                    child: Text('M'),
+                  ),
+                  SizedBox(width: 10),
+                  FloatingActionButton(
+                    onPressed: () {
+                      setState(() {
+                        city = 'Lahore';
+                      });
+                      _fetchWeatherData();
+                    },
+                    child: Text('L'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              FloatingActionButton(
+                onPressed: _fetchWeatherData,
+                child: Icon(Icons.auto_fix_normal_sharp),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Temperature:',
+                style: TextStyle(fontSize: 20),
+                selectionColor: Colors.redAccent,
+              ),
+              SizedBox(height: 16),
+              Text(
+                temperature,
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
 
-            ),
-            SizedBox(height: 16),
-            Text(
-              temperature,
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
